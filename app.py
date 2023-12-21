c7db9c3602e3cb48185270d0833e71fbbd083688
JudeInbinaryMankhanda <sajith.jude@flipick.com>
2023-12-21 23:49:48 +0530

    jsjsjsjs

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def main():
         gpt3_response = call_gpt3(st.session_state.full_prompt)
         print(gpt3_response)
         # if 'gpt3_response' not in st.session_state:
-        st.session_state.gpt3_response = str(gpt3_response)
+        st.session_state.gpt3_response = str(st.session_state.full_prompt)
         st.text_area("GPT-3.5 Response",st.session_state.gpt3_response, height=300)
     st.write(st.session_state.full_prompt)
     st.write(st.session_state.expenses_json)
